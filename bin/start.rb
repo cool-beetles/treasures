6d45c00cc33189a384267b68ec960dd54670539a
Ada <adrianna.kucinska@onet.eu>
2017-11-20 22:44:46 +0100

    #2 Fix 2 bugs. Add empty ditectory and fix relationship rental-treasure

--- bin/start.rb
+++ bin/start.rb
@@ -13,99 +13,120 @@ require '../lib/collections/types_collection.rb'
 require '../lib/collections/storages_collection.rb'
 require '../lib/collections/users_collection.rb'
 
-puts "user:"
-puts "your name"
-first_name = gets.chomp
-puts "your surname"
-last_name = gets.chomp
-puts "your age"
-age = gets.chomp
-user_1 = User.new(first_name, last_name, age)
-puts "your id: #{user_1.id}"
 
-puts "address:"
-puts "city"
-city = gets.chomp
-puts "street"
-street = gets.chomp
-puts "zip code"
-zip_code = gets.chomp
-address = Address.new(street, city, zip_code)
-puts "address it's storage"
-puts "storage name"
-storage_name = gets.chomp
-storage = Storage.new(storage_name)
-user_1.add_address(address)
-storage.add_address(address)
+#                !!!!!!!!!!!!!!!!!! Use if You need information about user !!!!!!!!!!!!!!!!!!!
+#------------------------------------------------------------------------------------------------------------
 
-puts "type of treasure"
-puts "type's name"
-type_name = gets.chomp
-type = Type.new(type_name)
-puts "id: #{type.id}"
 
-puts "add treasures"
-more_tresures = true
+# puts "user:"
+# puts "your name"
+# first_name = gets.chomp
+# puts "your surname"
+# last_name = gets.chomp
+# puts "your age"
+# age = gets.chomp
+# user_1 = User.new(first_name, last_name, age)
+# puts "your id: #{user_1.id}"
 
-while more_tresures do
-  puts "title"
-  title = gets.chomp
-  puts "description"
-  description = gets.chomp
-  user_1.add_treasure(type, title, description)
-  puts "Wanna add more? (y/n)"
-  more_tresures = gets.chomp == "y"
-end
+# puts "address:"
+# puts "city"
+# city = gets.chomp
+# puts "street"
+# street = gets.chomp
+# puts "zip code"
+# zip_code = gets.chomp
+# address = Address.new(street, city, zip_code)
+# puts "address it's storage"
+# puts "storage name"
+# storage_name = gets.chomp
+# storage = Storage.new(storage_name)
+# user_1.add_address(address)
+# storage.add_address(address)
 
-puts "Ok, here is a number of your treasuares: #{user_1.treasures.count}"
+# puts "type of treasure"
+# puts "type's name"
+# type_name = gets.chomp
+# type = Type.new(type_name)
+# puts "id: #{type.id}"
 
-puts "list of treasures:"
-puts user_1.treasures.map { |treasure| treasure.title }
-puts "-----------------------"
+# puts "add treasures"
+# more_tresures = true
 
-puts "friend's name"
-friends_name = gets.chomp
-puts "friend's surname"
-friends_surname = gets.chomp
-puts "friend's age"
-friends_age = gets.chomp
-friend = User.new(friends_name, friends_surname, friends_age)
-puts "friend's city"
-friends_city = gets.chomp
-puts "friend's street"
-friends_street = gets.chomp
-puts "friend's zip code"
-friends_zip_code = gets.chomp
-friends_address = Address.new(friends_street, friends_city, friends_zip_code)
-friend.add_address(friends_address)
+# while more_tresures do
+#   puts "title"
+#   title = gets.chomp
+#   puts "description"
+#   description = gets.chomp
+#   user_1.add_treasure(type, title, description)
+#   puts "Wanna add more? (y/n)"
+#   more_tresures = gets.chomp == "y"
+# end
 
-puts "rental"
-puts "Here is a list of your treasures with their ids, titles and descriptions:"
-user_1.treasures.each do |treasure| 
-  puts "#{treasure.id} #{treasure.title} #{treasure.description}"
-end
+# puts "Ok, here is a number of your treasuares: #{user_1.treasures.count}"
 
-puts "treasure id"
-treasure_id = gets.chomp
-puts "Until (YYYY-MM-DD):"
-due_date = gets.chomp
-Date.parse(due_date)
+# puts "list of treasures:"
+# puts user_1.treasures.map { |treasure| treasure.title }
+# puts "-----------------------"
 
-selected_treasure = user_1.treasures.detect { |treasure| treasure.id == treasure_id.to_i }
-friend.add_rental(selected_treasure, due_date)
+# puts "friend's name"
+# friends_name = gets.chomp
+# puts "friend's surname"
+# friends_surname = gets.chomp
+# puts "friend's age"
+# friends_age = gets.chomp
+# friend = User.new(friends_name, friends_surname, friends_age)
+# puts "friend's city"
+# friends_city = gets.chomp
+# puts "friend's street"
+# friends_street = gets.chomp
+# puts "friend's zip code"
+# friends_zip_code = gets.chomp
+# friends_address = Address.new(friends_street, friends_city, friends_zip_code)
+# friend.add_address(friends_address)
 
-puts "Done! #{friend.rentals.last.user.first_name} needs to return #{friend.rentals.last.treasure.title} before #{due_date}."
+# puts "rental"
+# puts "Here is a list of your treasures with their ids, titles and descriptions:"
+# user_1.treasures.each do |treasure| 
+#   puts "#{treasure.id} #{treasure.title} #{treasure.description}"
+# end
 
-TreasuresCollection.save
-UsersCollection.save
-AddressesCollection.save
-StoragesCollection.save
-TypesCollection.save
-RentalsCollection.save
+# puts "treasure id"
+# treasure_id = gets.chomp
+# puts "Until (YYYY-MM-DD):"
+# due_date = gets.chomp
+# Date.parse(due_date)
 
-# UsersCollection.load("../lib/collections/users_files")
-# StoragesCollection.load("../lib/collections/storages_files")
-# TypesCollection.load("../lib/collections/types_files")
-# TreasuresCollection.load("../lib/collections/treasures_files")
-# RentalsCollection.load("../lib/collections/rentals_files")
-# AddressesCollection.load("../lib/collections/addresses_files")
+# selected_treasure = user_1.treasures.detect { |treasure| treasure.id == treasure_id.to_i }
+# friend.add_rental(selected_treasure, due_date)
+
+# puts "Done! #{friend.rentals.last.user.first_name} needs to return #{friend.rentals.last.treasure.title} before #{due_date}."
+
+
+# TypesCollection.save
+# RentalsCollection.save
+# UsersCollection.save
+# AddressesCollection.save
+# StoragesCollection.save
+# TreasuresCollection.save
+
+ 
+
+#                                  !!!!!!!!!!!!!!!!! END !!!!!!!!!!!!!!!!
+#------------------------------------------------------------------------------------------------------------
+
+
+
+#               !!!!!!!!!!!!!!!!!! Use if You have files with user's data !!!!!!!!!!!!!!!!!! 
+#------------------------------------------------------------------------------------------------------------
+
+UsersCollection.load("../lib/collections/users_files")
+StoragesCollection.load("../lib/collections/storages_files")
+TypesCollection.load("../lib/collections/types_files")
+TreasuresCollection.load("../lib/collections/treasures_files")
+RentalsCollection.load("../lib/collections/rentals_files")
+AddressesCollection.load("../lib/collections/addresses_files")
+
+
+
+#                                    !!!!!!!!!!!!!!!!!! END !!!!!!!!!!!!!!!!
+#------------------------------------------------------------------------------------------------------------
